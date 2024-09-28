d7302fab79e9d231eead99bc4e6341905290f373
MateoVR13 <mattvr130@gmail.com>
2024-09-27 19:51:31 -0500

    Se agregó el ok y redireccionamento

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe y la página se recargue
+    event.preventDefault();
     
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Validación de credenciales
+s
     if (email === 'admin@gmail.com' && password === 'admin') {
-        // Si las credenciales son correctas, redirige a la página de registro
+
         window.location.href = 'register.html';
     } else {
-        // Si las credenciales son incorrectas, muestra un mensaje de error
+
         document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos';
     }
 });
